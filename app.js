733b8de8d0f885538a411dcfbf1f396bcf436eac
PacoCanovaca <pacocanovaca97@gmail.com>
2025-08-29 12:38:13 +0200

    Botón de borrado programado

--- app.js
+++ app.js
@@ -5,64 +5,78 @@ let operator = "";
 let result = "Error";
 
 const numberScreen = () => {
-    const numberButtons = document.querySelectorAll(".number");
-    for (let i = 0; i < numberButtons.length; i++) {
-        const numberButton = numberButtons[i];
-        numberButton.addEventListener("click", () => {
-        if (screen.innerText.length < 11) {
-            if (screen.innerText === "+" || screen.innerText === "−" || screen.innerText === "×" || screen.innerText === "÷") {
-                screen.innerText = numberButton.innerText
-            } else {
-                screen.innerText = screen.innerText + numberButton.innerText
-            }
+  const numberButtons = document.querySelectorAll(".number");
+  for (let i = 0; i < numberButtons.length; i++) {
+    const numberButton = numberButtons[i];
+    numberButton.addEventListener("click", () => {
+      if (screen.innerText.length < 11) {
+        if (
+          screen.innerText === "+" ||
+          screen.innerText === "−" ||
+          screen.innerText === "×" ||
+          screen.innerText === "÷"
+        ) {
+          screen.innerText = numberButton.innerText;
+        } else {
+          screen.innerText = screen.innerText + numberButton.innerText;
         }
-        })
-    }
-    
-}
+      }
+    });
+  }
+};
 
 const selectOperator = () => {
-    const operatorButtons = document.querySelectorAll(".operators");
-    for (const operatorButton of operatorButtons) {
-        operatorButton.addEventListener("click", () => {
-            firstNumber = parseInt(screen.innerText)
-            screen.innerText = operatorButton.innerText
-            operator = operatorButton.innerText
-        })
-    }
-}
+  const operatorButtons = document.querySelectorAll(".operators");
+  for (const operatorButton of operatorButtons) {
+    operatorButton.addEventListener("click", () => {
+      firstNumber = parseFloat(screen.innerText);
+      screen.innerText = operatorButton.innerText;
+      operator = operatorButton.innerText;
+    });
+  }
+};
 
 const solveOperation = () => {
-    const equalButton = document.querySelector(".equal");
-    equalButton.addEventListener("click", () => {
-        secondNumber = parseInt(screen.innerText)
-        if (operator === "+") {
-            result = firstNumber + secondNumber
-        } else if (operator === "−") {
-            result = firstNumber - secondNumber
-        } else if (operator === "×") {
-            result = firstNumber * secondNumber
-        } else if (operator === "÷") {
-            result = firstNumber / secondNumber
-        }
-        screen.innerText = String(result);
-        firstNumber = result;
-        secondNumber = 0;
-    })
-}
+  const equalButton = document.querySelector(".equal");
+  equalButton.addEventListener("click", () => {
+    secondNumber = parseInt(screen.innerText);
+    if (operator === "+") {
+      result = firstNumber + secondNumber;
+    } else if (operator === "−") {
+      result = firstNumber - secondNumber;
+    } else if (operator === "×") {
+      result = firstNumber * secondNumber;
+    } else if (operator === "÷") {
+      result = firstNumber / secondNumber;
+    }
+    screen.innerText = String(result);
+    firstNumber = result;
+    secondNumber = 0;
+  });
+};
 
 const deleteAllScreen = () => {
-    const deleteAllButton = document.querySelector("#deleteAll");
-    deleteAllButton.addEventListener("click", () => {
-        screen.innerText = "";
-        firstNumber = 0;
-        secondNumber = 0;
-        operator = "";
-    })
-}
+  const deleteAllButton = document.querySelector("#deleteAll");
+  deleteAllButton.addEventListener("click", () => {
+    screen.innerText = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = "";
+  });
+};
 
-numberScreen()
-selectOperator()
-solveOperation()
-deleteAllScreen()
+const deleteOne = () => {
+  const deleteOneButton = document.querySelector("#delete");
+  deleteOneButton.addEventListener("click", () => {
+    const screenList = screen.innerText.split("");
+    const lastNum = screenList[screenList.length - 1];
+    screenList.pop();
+    screen.innerText = screenList.join("");
+  });
+};
 
+numberScreen();
+selectOperator();
+solveOperation();
+deleteAllScreen();
+deleteOne();
